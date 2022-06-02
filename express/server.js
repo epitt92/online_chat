5dcd0dbcfd57ae95cb53b052aa2efb9e31f1bda1
memokey <skytopground@gmail.com>
2022-06-02 13:21:43 +0800

    'server.js'

--- express/server.js
+++ express/server.js
@@ -24,7 +24,7 @@ const io             = socketIO(server);
 require("./io/index")(io);
 
 // Configure app and mongoose
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/express/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
